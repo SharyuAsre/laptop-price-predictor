41b27daab145970fa4e94a3b9f29d795bc61a997
SharyuAsre <sharyuasre169@gmail.com>
2025-10-10 19:29:59 +0530

    first commit

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 MONGO_URI = "mongodb+srv://sharyuasre169_db_user:panda169@cluster0.uysrodh.mongodb.net/"  # <-- replace with your URI
-DATABASE_NAME = "aiml"       # <-- replace with your DB name
+DATABASE_NAME = "aiml"       # <-- replace with your DB namept
 COLLECTION_NAME = "laptop_price" 
 
 client = MongoClient(MONGO_URI)
@@ -12,5 +12,6 @@ db = client[DATABASE_NAME]
 collection = db[COLLECTION_NAME]
 data = list(collection.find())
 df = pd.DataFrame(data)
+OUTPUT_PATH = "data/raw_data.csv"
 #df.to_csv(OUTPUT_PATH, index=False)
 print(f"✅ Loaded {len(df)} records and saved to {OUTPUT_PATH}")
